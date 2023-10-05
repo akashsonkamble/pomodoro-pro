521e77c916504c32d700df2342c8f85fda8b0794
akash <akashsonkamble199@gmail.com>
2023-10-05 20:06:08 +0530

    chore(index): Import components into index.js

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from "../components/utils/Container";
+import Logo from "../components/utils/Logo"
+import Button from "../components/utils/Button";
+
+export {
+    Header,
+    LogoutBtn,
+    Container,
+    Logo,
+    Button
+}
