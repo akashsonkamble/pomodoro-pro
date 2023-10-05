57f55fcb9eb17f508b51a5fc7bff5244a3d613b1
akash <akashsonkamble199@gmail.com>
2023-10-05 20:04:22 +0530

    feat(components): Add Header and LogoutBtn components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,59 @@
+import { LogoutBtn, Container, Logo, Button } from "./index";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+    const navigate = useNavigate();
+    const authStatus = useSelector((state) => state.auth.status);
+
+    const navItems = [
+        {
+            name: "Home",
+            path: "/",
+            active: true,
+        },
+        {
+            name: "Login",
+            path: "/login",
+            active: !authStatus,
+        },
+        {
+            name: "Signup",
+            path: "/signup",
+            active: !authStatus,
+        },
+    ]
+
+    return (
+        <header className="py-3 bg-#d2e2f2 border-b-2 border-500">
+            <Container>
+                <nav className="flex">
+                    <div className="mr-4">
+                        <Link to="/">
+                            <Logo width="150px" />
+                        </Link>
+                    </div>
+                    <ul className="flex ml-auto">
+                        {navItems.map((item) => (
+                            item.active ? (
+                                <li key={item.name}>
+                                    <Button
+                                        onClick={() => navigate(item.path)}
+                                        className="inline-block mr-4 px-6 py-2 duration-200 rounded-full"
+                                    ></Button>
+                                </li>
+                            ) : null
+                        ))}
+                        {authStatus && (
+                            <li>
+                                <LogoutBtn />
+                            </li>
+                        )}
+                    </ul>
+                </nav>
+            </Container>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,30 @@
+import Button from "../Button";
+import authService from "../../appwrite/auth"
+import { logout } from "../../store/authSlice"
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const LogoutBtn = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const logoutHandler = () => {
+        authService.logout()
+        .then(() => {
+            dispatch(logout());
+            navigate("/");
+        })
+        .catch((error) => {
+            console.log("LogoutBtn :: logoutHandler :: error", error);
+        })
+    }
+    return (
+        <Button
+            className="inline-block mr-4 px-6 py-2 duration-200 rounded-full"
+            onClick={logoutHandler}
+        >Logout
+        </Button>
+    )
+}
+
+export default LogoutBtn;
