f69a118102c53cca15210aac5e8768961f33a6b5
akash <akashsonkamble199@gmail.com>
2023-10-10 21:20:21 +0530

    feat(main): Implement routing in main.jsx component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from "./pages/Home.jsx";
+import LoginPage from "./pages/Login.jsx";
+import SignupPage from "./pages/Signup.jsx";
+
+import "react-toastify/dist/ReactToastify.css";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <LoginPage />
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <SignupPage />
+          </AuthLayout>
+        )
+      }
+    ],
+  },
+  
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
