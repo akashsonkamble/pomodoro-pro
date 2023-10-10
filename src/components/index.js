8b05bc8b46cad9644a297b684b49e4ab6d8cc916
akash <akashsonkamble199@gmail.com>
2023-10-10 20:10:03 +0530

    chore(index): Import components into index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,17 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Container from "../components/utils/Container";
 import Logo from "../components/utils/Logo"
 import Button from "../components/utils/Button";
+import Login from "../components/Login";
+import Signup from "../components/Signup";
+import Pomodoro from "../components/Pomodoro";
 
 export {
     Header,
     LogoutBtn,
     Container,
     Logo,
-    Button
+    Button,
+    Login,
+    Signup,
+    Pomodoro
 }
