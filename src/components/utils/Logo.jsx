d43f8db0495f556494192edb741b8997776c53f0
akash <akashsonkamble199@gmail.com>
2023-10-11 00:10:45 +0530

    chore: Move favicon and logo to the public folder

--- src/components/utils/Logo.jsx
+++ src/components/utils/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = ({ width = "150px"}) => {
     return (
         <div>
-            <img src="./src/assets/logo.png" width={width} className="inline" alt="logo" />
+            <img src="/logo.png" width={width} className="inline" alt="logo" />
         </div>
     )
 }
